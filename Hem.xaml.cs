9f6b7a14aa8d5f5708d2038aa0f02e45d3766e8c
Mattias <walde87@msn.com>
2023-10-04 22:18:55 +0200

    Massvis med försök, fungerar inte än

--- Hem.xaml.cs
+++ Hem.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using System.Globalization;
+using System.Net.Http;
 
 namespace MatchManiaWPF
 {
@@ -23,6 +25,8 @@ namespace MatchManiaWPF
     public partial class Hem : Window
     {
         public List<Match> Matches { get; set; }
+        public List<Match> FirstFiveMatches { get; set; }
+
         public Hem()
         {
             InitializeComponent();
@@ -31,21 +35,25 @@ namespace MatchManiaWPF
         }
         private void LoadMatches()
         {
+            string fileName = "jsonTestResponse.json";
+            string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
             try
             {
-                string path = "jsonTestResponse.json";
-                string jsonText = File.ReadAllText(path);
+                string jsonText = File.ReadAllText(filePath);
                 var json = JsonConvert.DeserializeObject<RootObject>(jsonText);
                 Matches = json.response.Select(matchData => new Match
-                    {
-                        Lag1Name = matchData.teams.home.name,
-                        Lag2Name = matchData.teams.away.name,
-                        Lag1Score = matchData.goals.home,
-                        Lag2Score = matchData.goals.away,
-                        Lag1Logo = matchData.teams.home.logo,
-                        Lag2Logo = matchData.teams.away.logo,
-                        Datum = DateTime.Parse(matchData.fixture.date)
-                    }).ToList();
+                {
+                    Lag1Name = matchData.teams.home.name,
+                    Lag2Name = matchData.teams.away.name,
+                    Lag1Score = matchData.goals.home,
+                    Lag2Score = matchData.goals.away,
+                    Lag1Logo = matchData.teams.home.logo,
+                    Lag2Logo = matchData.teams.away.logo,
+                }).ToList();
+
+                // Hämta de fem första matcherna
+                FirstFiveMatches = Matches.Take(5).ToList();
             }
             catch (Exception ex)
             {
@@ -65,6 +73,11 @@ namespace MatchManiaWPF
             /// Skapa en string-lista från json-filen och eventuellt med en <ItemsControl> skapar vi en UI för att visa matchinformationen. 
             /// Logotyper hämtas troligtvis via http från någon databas och dessa visas som <Image Source="lagx" Height="" Width=""/> tillsammans med <TextBlock/>
             /// Detta kan räcka för att få till en dräglig lösning för att visa kommande matcher.
+        }
+        private void ResultatKlick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Antal matcher i FirstFiveMatches: {FirstFiveMatches.Count}");
+            ResultatItemsControl.ItemsSource = FirstFiveMatches;
         }
         private void TillbakaKlick(object sender, RoutedEventArgs e)
         {
@@ -87,12 +100,88 @@ namespace MatchManiaWPF
         {
             public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
             {
-                if(value is List<Match> matches)
+                if (value is List<Match> matches)
                 {
                     var sortedMatches = matches.OrderBy(match => match.Datum).ToList();
                     var nextMatch = sortedMatches.FirstOrDefault(match => match.Datum > DateTime.Now);
+                    return nextMatch;
                 }
+                return null;
+            }
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
             }
         }
+        public class RootObject
+        {
+            public string get { get; set; }
+            public Parameters parameters { get; set; }
+            public List<object> errors { get; set; }
+            public int results { get; set; }
+            public List<ResponseItem> response { get; set; }
+        }
+
+        public class Parameters
+        {
+            public string league { get; set; }
+            public string last { get; set; }
+            public string season { get; set; }
+        }
+
+        public class ResponseItem
+        {
+            public Fixture fixture { get; set; }
+            public League league { get; set; }
+            public Teams teams { get; set; }
+            public Goals goals { get; set; }
+        }
+
+        public class Fixture
+        {
+            public int id { get; set; }
+            public string referee { get; set; }
+            public string timezone { get; set; }
+            public DateTime date { get; set; }
+            public int timestamp { get; set; }
+            public Venue venue { get; set; }
+        }
+
+        public class Venue
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string city { get; set; }
+        }
+
+        public class League
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string country { get; set; }
+            public string logo { get; set; }
+            public string round { get; set; }
+        }
+
+        public class Teams
+        {
+            public Team home { get; set; }
+            public Team away { get; set; }
+        }
+
+        public class Team
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string logo { get; set; }
+            public bool winner { get; set; }
+        }
+
+        public class Goals
+        {
+            public int home { get; set; }
+            public int away { get; set; }
+        }
+
     }
 }
