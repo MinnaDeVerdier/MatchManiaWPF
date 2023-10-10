f567f7be2bd9e5abebac7c60a84278248dbb8b67
Mattias <walde87@msn.com>
2023-10-10 09:22:49 +0200

    Refaktorering i matchsidan

--- Matchsida.xaml.cs
+++ Matchsida.xaml.cs
@@ -22,7 +22,12 @@ namespace MatchManiaWPF
         public Matchsida(string lag1, string lag2, string logo1, string logo2, int mål1, int mål2)
         {
             InitializeComponent();
+            InitMatchsidaSpeladMatch(lag1, lag2, logo1, logo2, mål1, mål2);
 
+        }
+
+        private void InitMatchsidaSpeladMatch(string lag1, string lag2, string logo1, string logo2, int mål1, int mål2)
+        {
             Lag1Namn.Text = lag1;
             Lag2Namn.Text = lag2;
             BitmapImage logoHemma = new BitmapImage(new Uri(logo1, UriKind.RelativeOrAbsolute));
@@ -31,13 +36,9 @@ namespace MatchManiaWPF
             Lag2Logo.Source = logoBorta;
             Lag1Score.Text = mål1.ToString();
             Lag2Score.Text = mål2.ToString();
-            
         }
 
-    /// Kod som tar tidigare vald match och visar mer information om matchen,
-    /// kärndata från det förra fönstret måste hit och visas på ett snyggt sätt i
-    /// ett eget fönster.
-    /// Resultat = statistik osv.
-    /// Kommande = Odds, arena, väder(?!) dommare osv.
+        /// Resultat = score, statistik osv.
+        /// Kommande = Odds, arena, väder(?!) dommare osv.
     }
 }
