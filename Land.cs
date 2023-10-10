f4cd51c072c274d8c71154edcc8d80d9597208ec
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-10-10 09:56:05 +0200

    Lagt till combobox-lista för länder

--- Land.cs
+++ Land.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace MatchManiaWPF
 {
@@ -22,8 +23,9 @@ namespace MatchManiaWPF
             public string flag { get; set; }
             public string name { get; set; }
         }
-        public static void SearchCountries(Rootobject länder)
+        public static Rootobject SearchCountries()
         {
+            Rootobject länder = new();
             string dir = @"..\..\..\";
             string fileName = "CountriesResponseAll.json";
             string path = System.IO.Path.Combine(dir, fileName);
@@ -33,11 +35,29 @@ namespace MatchManiaWPF
                 string jsonText = File.ReadAllText(path);
                 länder = JsonConvert.DeserializeObject<Rootobject>(jsonText, nullIgnore);
 
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+            if (länder != null)
+            {
+                //MessageBox.Show("succe objekt länder");
+                return länder;
+            }
+            else
+            {
+                MessageBox.Show("fel objekt länder");
+                return länder;
+            }
+        }
+        public static List<string> CountryNames(Rootobject länder)
+        {
+            List<string> länderNamn = new();
+            foreach (Country c in länder.response)
+                länderNamn.Add(c.name);
+            return länderNamn;
         }
     }
 
