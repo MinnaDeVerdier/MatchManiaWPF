a3744ff7d9aba4732809ab23c7570cd1b896d302
Mattias <walde87@msn.com>
2023-10-10 10:03:59 +0200

    Pages fix.

--- Matchsida.xaml.cs
+++ Matchsida.xaml.cs
@@ -22,19 +22,7 @@ namespace MatchManiaWPF
         public Matchsida(string lag1, string lag2, string logo1, string logo2, int mål1, int mål2)
         {
             InitializeComponent();
-            InitMatchsidaSpeladMatch(lag1, lag2, logo1, logo2, mål1, mål2);
-        }
-
-        private void InitMatchsidaSpeladMatch(string lag1, string lag2, string logo1, string logo2, int mål1, int mål2)
-        {
-            Lag1Namn.Text = lag1;
-            Lag2Namn.Text = lag2;
-            BitmapImage logoHemma = new BitmapImage(new Uri(logo1, UriKind.RelativeOrAbsolute));
-            Lag1Logo.Source = logoHemma;
-            BitmapImage logoBorta = new BitmapImage(new Uri(logo2, UriKind.RelativeOrAbsolute));
-            Lag2Logo.Source = logoBorta;
-            Lag1Score.Text = mål1.ToString();
-            Lag2Score.Text = mål2.ToString();
+            Main.Navigate(new SpeladeMatchsida(lag1, lag2, logo1, logo2, mål1, mål2));
         }
 
         /// Resultat = score, statistik osv.
